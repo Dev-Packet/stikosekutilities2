18481e9f21899a372fe3769f5525d5889ef952b8
stikosek <stikosek@DESKTOP-Q0OMASV>
2021-12-13 20:26:38 +0100

    Přidejte soubory projektu.

---
+++ stikosekutilities2/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using UnityEngine;
+
+namespace stikosekutilities2
+{
+    [BepInPlugin("devpacket.stikosekutilities2", "stikosekutilities2", "0.0.0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin stikosekutilities2 is loaded!");
+        }
+    }
+}
