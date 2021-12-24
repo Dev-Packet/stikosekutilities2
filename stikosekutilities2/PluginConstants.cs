ee2c63e679abfa97aed839a21469b68b9f78c78e
CodeName-Anti <bamblampel@gmail.com>
2021-12-24 11:43:02 +0100

    - Fix

--- stikosekutilities2/PluginConstants.cs
+++ stikosekutilities2/PluginConstants.cs
@@ -10,7 +10,7 @@
 
         // GitHub related
         public const string
-            RepositoryOwner = "DevPacket",
+            RepositoryOwner = "Dev-Packet",
             Repository = "stikosekutilities2",
             RepositoryURL = $"https://github.com/{RepositoryOwner}/{Repository}",
             ReleasesAPI = $"https://api.github.com/repos/{RepositoryOwner}/{Repository}/releases";
